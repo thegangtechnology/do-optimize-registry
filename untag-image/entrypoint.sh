a37d8bd15c767c0b80dbc2bac2e42efae11a3998
dawit-cht <dawit.cht@gmail.com>
2024-07-19 16:30:15 +0700

    init: untag image action

--- untag-image/entrypoint.sh
+++ untag-image/entrypoint.sh
@@ -15,10 +15,10 @@ doctl auth init -t "$DO_TOKEN" || { echo "Authentication failed"; exit 1; }
 
 # Create list of image that will be untagged
 echo "Create untagged list..."
-doctl registry repository lm "$REGISTRY_NAME" --format Digest,Tags,UpdatedAt  | grep '\[sha[-0-9a-z]*\]' > tag-list.txt
+doctl registry repository lm "$REPOSITORY_NAME" --format Digest,Tags,UpdatedAt  | grep '\[sha[-0-9a-z]*\]' > tag-list.txt || { echo "Cannot get image list"; exit 1; }
 awk '{print $1, $2, $3}' tag-list.txt > normalize-list.txt
 cat normalize-list.txt
 
 # Run the Python script with any arguments passed to the container
 echo "Executing Python script..."
-exec python3 untag-image.py --repo $REGISTRY_NAME "$@"
+exec python3 untag-image.py --repo $REPOSITORY_NAME "$@"
