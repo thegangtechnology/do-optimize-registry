565cd205f0d31d9a8090a1ffdd19dfce85e6dcfa
boatpand <boat@Tanapats-MacBook-Air.local>
2024-11-01 18:00:49 +0700

    add condition and optimize for untagging docker image

--- untag-image/entrypoint.sh
+++ untag-image/entrypoint.sh
@@ -15,10 +15,14 @@ doctl auth init -t "$DO_TOKEN" || { echo "Authentication failed"; exit 1; }
 
 # Create list of image that will be untagged
 echo "Create untagged list..."
+# for general
 doctl registry repository lm "$REPOSITORY_NAME" --format Digest,Tags,UpdatedAt  | grep '\[sha[-0-9a-z]*\]' > tag-list.txt || { echo "Cannot get image list"; exit 1; }
 awk '{print $1, $2, $3}' tag-list.txt > normalize-list.txt
 cat normalize-list.txt
+# for keeping n latest version tags
+doctl registry repository lm "$REPOSITORY_NAME" --format Digest,Tags,UpdatedAt | grep sha256 | grep -v '\[sha[-0-9a-z]*\]' | grep -v '\[\]' > version-tag-list.txt || { echo "Cannot get image list"; exit 1; }
+awk -F '\s{2,}' '{print $1, $2, $3}' version-tag-list.txt > normalize-version-list.txt
 
 # Run the Python script with any arguments passed to the container
 echo "Executing Python script..."
-python3 /app/untag-image.py --repo $REPOSITORY_NAME
+python3 /app/untag-image.py --repo $REPOSITORY_NAME --days $DAYS --keep-latest $KEEP_LATEST
--- untag-image/untag-image.py
+++ untag-image/untag-image.py
@@ -2,30 +2,47 @@
 import argparse
 import datetime
 import subprocess
-
-filename = "normalize-list.txt"
-weekago = datetime.datetime.now() - datetime.timedelta(days=7)
-format = "%Y-%m-%d"
+import re
 
 parser = argparse.ArgumentParser("simple_example")
 parser.add_argument("--repo", dest="repository", help="A repository on Digital Ocean Container Registry", type=str)
+parser.add_argument("--days", dest="days" ,help="Number of days for keeping images. Default to 0 if specific keep-latest,  default to 7 if not specific keep-latest", type=int)
+parser.add_argument("--keep-latest", dest="keep_latest_tags", help="Untag versions from imgages older than keep_latest images, Default to 0 (Disable)", type=int)
 args = parser.parse_args()
 count = 1
+num_latest_image = 3
+
+if not args.days:
+    if args.keep_latest_tags:
+        args.days = 0
+    else:
+        args.days = 7
+
+keep_days = datetime.datetime.now() - datetime.timedelta(days=args.days)
+format = "%Y-%m-%d"
+
+def untag_images(num, count):
+    tags = re.search(r'\[(.*?)\]', str(line)).group(0)
+    if count <= num:
+        print(f"keep tag: {tags} as {num} latest image")
+        count += 1
+    else:
+        data_time = datetime.datetime.strptime(re.search(r'\d{4}-\d{2}-\d{2}', str(line)).group(0), format)
+        if count == num + 1:
+            print(f"!!! images that updated before {keep_days} will be removed !!!")
+            count += 1
+        if data_time < keep_days:
+            # subprocess.run(["echo", "" +data[1] + " " +data[2]])
+            print(f"Untag {tags} from repository")
+            subprocess.run(["doctl", "registry", "repository", "dt", args.repository, tags, "-f"])
+        else:
+            print(f"tag: {tags} on {data_time} is not outdated yet")
+    return True
+
+filename = "normalize-version-list.txt" if args.keep_latest_tags else "normalize-list.txt"
+num = args.keep_latest_tags if args.keep_latest_tags else num_latest_image
 
 with open(filename) as file:
     while line := file.readline():
-        if count <= 5:
-            data = line.rstrip().split()
-            data[1] = data[1][1:-1]
-            print(f"keep tag:{data[1]} as 5 latest image")
-            count += 1
-        else:
-            data = line.rstrip().split()
-            data[1] = data[1][1:-1]
-            data_time = datetime.datetime.strptime(data[2], format)
-            if data_time < weekago:
-                # subprocess.run(["echo", "" +data[1] + " " +data[2]])
-                print(f"Untag {data[1]} from repository")
-                subprocess.run(["doctl", "registry", "repository", "dt", args.repository, data[1], "-f"])
-            else:
-                print(f"tag: {data[1]} on {data[2]} is not outdated yet")
+        untag_images(num, count)
+        count +=1
