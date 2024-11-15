5df16a2c98c27026814a56c3c4b8f041bbd00654
dawit-cht <dawit.cht@gmail.com>
2024-11-15 16:58:16 +0700

    feat: add latest tag and 5 older version

--- untag-image/untag-image.py
+++ untag-image/untag-image.py
@@ -48,6 +48,7 @@ def keep_deployed_version(tag_list=[], untag_list=[], version_list=[]):
             for i in range(5):
                 keep_list.append(tag_list[index_keep + i])
     print(f"keep version list : {keep_list}")
+    print(f"keep extra {len(keep_list)} versions")
     # remove keep version from untag list
     for keep_version in keep_list:
         if keep_version in untag_list:
@@ -56,8 +57,9 @@ def keep_deployed_version(tag_list=[], untag_list=[], version_list=[]):
 
 
 # create list of old image
-def create_untag_images_list(file, keep_days=None, keep_tags=None, tag_list=None):
+def create_untag_images_list(file, keep_days=None, keep_tags=None, tag_list=None, version_list=None):
     # setup data
+    latest_tag = ""
     untag_list = []
     date_format = "%Y-%m-%d"
     with open(file) as file:
@@ -78,20 +80,24 @@ def create_untag_images_list(file, keep_days=None, keep_tags=None, tag_list=None
                     untag_list.append(tags[-1])
                 else:
                     print(f"tag: {tags[-1]} on {data_time} is not outdated yet")
+            for tag in tags:
+                if tag == "latest":
+                    version_list.append(tags[-1])
+                    print(f"{tags[-1]} will not be deleted")
     return untag_list
 
 # Run doctl to untag image
 def untag_image(untag_list, repository):
     for image in untag_list:
         print(f"untag: {image}")
-        subprocess.run(["doctl", "registry", "repository", "dt", repository, image, "-f"])
+        # subprocess.run(["doctl", "registry", "repository", "dt", repository, image, "-f"])
 
 
 # Main Execution Function
 def main():
     # Setup variables
     parser = argparse.ArgumentParser("simple_example")
-    parser.add_argument("--repo", dest="repository", default="backend",
+    parser.add_argument("--repo", dest="repository",
                         help="A repository on Digital Ocean Container Registry", type=str)
     parser.add_argument("--keep-sha", dest="days", default=7,
                         help="Number of days for keeping images. Default to 0 if specific keep-latest,  default to 7 "
@@ -106,19 +112,14 @@ def main():
     tag_list = []
 
     # Set keep threshold and untag image
-    # Untag only sha images
-    if args.days is not None:
-        keep_days = datetime.datetime.now() - datetime.timedelta(days=args.days)
-        sha_filename = "normalize-list.txt"
-        untag_sha_list = create_untag_images_list(sha_filename, keep_days=keep_days)
 
     # # Untag version images
     if args.months is not None:
         keep_tags = datetime.datetime.now() - relativedelta(months=args.months)
-        # version_list = get_version(repository=args.repository, staging_url=staging_url, production_url=production_url)
-        version_list = ['0.2.0-4', '0.1.10']
+        version_list = get_version(repository=args.repository, staging_url=staging_url, production_url=production_url)
+        #version_list = ['0.2.0-4', '0.1.10']
         version_filename = "normalize-version-list.txt"
-        untag_version_list = create_untag_images_list(version_filename, keep_tags=keep_tags, tag_list=tag_list)
+        untag_version_list = create_untag_images_list(version_filename, keep_tags=keep_tags, tag_list=tag_list, version_list=version_list)
         # Sort list
         tag_list.remove("develop")
         sorted_tag = sorted(tag_list, key=parse_version, reverse=True)
@@ -127,11 +128,17 @@ def main():
         # Remove deployed tag from untag list
         final_list = keep_deployed_version(tag_list=sorted_tag, untag_list=sorted_untag_versions_list, version_list=version_list)
 
+    # Untag only sha images
+    if args.days is not None:
+        keep_days = datetime.datetime.now() - datetime.timedelta(days=args.days)
+        sha_filename = "normalize-list.txt"
+        untag_sha_list = create_untag_images_list(sha_filename, keep_days=keep_days)
+
     # Untag image
-    print(f"untag sha list: {untag_sha_list}")
-    untag_image(untag_sha_list, args.repository)
     print(f"untag version list: {sorted_untag_versions_list}")
     untag_image(final_list, args.repository)
+    print(f"untag sha list: {untag_sha_list}")
+    untag_image(untag_sha_list, args.repository)
 
 
 # Standard Script Entry Point
